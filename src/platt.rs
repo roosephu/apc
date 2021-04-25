63babd16c13d02498aaa1e6c1d743dda5a306a09
Yuping Luo <roosephu@gmail.com>
2021-04-25 00:19:44 -0400

    approx ln(u/x)/lambda in f64

--- src/platt.rs
+++ src/platt.rs
@@ -59,19 +59,25 @@ impl<T: MyReal> Platt<T> {
 
     #[inline(never)]
     fn sieve(primes: &[u64], l: u64, r: u64) -> Vec<u64> {
-        let mut mark = bit_vec::BitVec::from_elem((r - l + 1) as usize, false);
+        let ub = (r - l) as usize;
+        let mut mark = bit_vec::BitVec::from_elem(ub + 1, false);
         for &p in primes {
-            let mut x = std::cmp::max((l - 1) / p + 1, 2) * p;
-            while x <= r {
-                mark.set((x - l) as usize, true);
-                // might overflow!
-                x += p;
+            let x = std::cmp::max((l - 1) / p + 1, 2) * p;
+            let mut y = (x - l) as usize;
+            while y <= ub {
+                mark.set(y, true);
+                y += p as usize;
             }
         }
         let mut ret = vec![];
-        for x in l..=r {
-            if !mark[(x - l) as usize] {
+        for (idx, &s) in mark.storage().iter().enumerate() {
+            let mut s = !s;
+            while s != 0 {
+                let w = s & (s - 1);
+                let offset = (s ^ w).trailing_zeros();
+                let x = l + ((idx as u64) << 5) + offset as u64;
                 ret.push(x);
+                s = w;
             }
         }
 
@@ -117,21 +123,30 @@ impl<T: MyReal> Platt<T> {
     #[inline(never)]
     fn calc_delta_f64(&self, x: u64, eps: f64) -> T {
         let mut ret = 0.0;
-        let fx = (x as i64).unchecked_cast::<T>();
+        let fx = x as f64;
         let (x1, x2) = (self.x1, self.x2);
         let eps = eps / ((x2 - x1 + 1) + x2.sqrt() + 1) as f64;
         info!("delta eps = {:.e}", eps);
+        let lambda: f64 = self.lambda.unchecked_cast();
 
-        fn Phi(r: f64) -> f64 {
-            rgsl::error::erfc(r / std::f64::consts::SQRT_2) / 2.0
-        }
+        fn Phi(r: f64) -> f64 { rgsl::error::erfc(r / std::f64::consts::SQRT_2) / 2.0 }
+
+        let w = lambda * x as f64;
+        let c1 = -1.0 / (lambda * w);
+        let c2 = -0.5 / (lambda * w * w);
+        let c3 = -1.0 / 3.0 / (lambda * w * w * w);
 
         // error analysis: we have ~(x2 - x1)/log(x) many p's.
         // for each p: the error by erfc is delta.
         let primes = Self::linear_sieve(x2.sqrt());
         for p in Self::sieve(&primes, x1, x2) {
-            let r = ((p as i64).unchecked_cast::<T>() / fx).ln() / self.lambda;
-            let r = r.unchecked_cast::<f64>();
+            // here we approximate r = ln(u / x) / lambda.
+            // Define t = lambda (x - u), and we have r = ln(1 - t/(x lambda)) / lambda.
+            // typically t is not large, while x lambda is very large
+            // Expad ln(1 - t / (x lambda)) at t = 0.
+            // since x - p = O(sqrt(x)) so there is no precision loss.
+            let t = (x as i64 - p as i64) as f64 * lambda;
+            let r = c1 * t + c2 * t * t + c3 * t * t * t;
             ret -= Phi(r);
             if p <= x {
                 ret += 1.0;
@@ -139,7 +154,6 @@ impl<T: MyReal> Platt<T> {
         }
 
         // error analysis: each has error delta, we have sqrt(x2) many, so in total is $delta sqrt(x2) << 1$.
-        let lambda: f64 = self.lambda.unchecked_cast();
         for p in primes {
             let mut m = 1i64;
             let mut power = p;
@@ -151,7 +165,7 @@ impl<T: MyReal> Platt<T> {
                 } else {
                     // only (x2^1/2 - x1^1/2) + (x2^1/3 - x1^1/3) + ... many
                     // The first term dominates, which is still O((x2 - x1)/sqrt(x)) = O(1).
-                    let r = ((power as f64) / (x as f64)).ln() / lambda;
+                    let r = ((power as f64) / fx).ln() / lambda;
                     ret -= Phi(r) / m as f64;
                 }
             }
@@ -168,7 +182,13 @@ impl<T: MyReal> Platt<T> {
         let (x1, x2) = self.plan_delta_bounds(lambda, x, 0.24);
         let integral_limit = self.plan_integral(lambda, x, 0.1);
         info!("lambda = {:.6}", lambda);
-        info!("delta range = [{}, {}], length = {}, est = {:.0}", x1, x2, x2 - x1, 2.0 * lambda * x * (2.0 * (lambda * x).ln()).sqrt());
+        info!(
+            "delta range = [{}, {}], length = {}, est = {:.0}",
+            x1,
+            x2,
+            x2 - x1,
+            2.0 * lambda * x * (2.0 * (lambda * x).ln()).sqrt()
+        );
         info!("integral limit = {:.6}", integral_limit,);
 
         self.lambda = lambda.unchecked_cast();
