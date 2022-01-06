5c85f53e502f2a92c624f94364b4a49a93fb66d6
Yuping Luo <roosephu@gmail.com>
2022-01-06 03:22:15 -0500

    rename variables

--- src/bandwidth_interp.rs
+++ src/bandwidth_interp.rs
@@ -5,22 +5,6 @@ use crate::sum_trunc_dirichlet::sum_trunc_dirichlet;
 use crate::traits::MyReal;
 use log::debug;
 
-/// a data structure for querying \sum_{t=1}^k n^{-sigma - i t}
-pub struct BandwidthInterp<T: MyReal, K: Kernel<T> = SincKernel<T>> {
-    k0: usize,
-    k1: usize,
-    tau: T,
-    sigma: T,
-    t0: T,
-    gap: T,
-
-    alpha: T,
-    beta: T,
-    delta: T,
-    data: Vec<Complex<T>>,
-    h: K,
-}
-
 /// determine c: c / sinh(c) = eps
 fn find_c(eps: f64) -> f64 {
     let c = 1.0;
@@ -29,20 +13,20 @@ fn find_c(eps: f64) -> f64 {
     c
 }
 
-fn interpolate<T: MyReal>(data: &[Complex<T>], x: T, beta: T, h: &impl Kernel<T>) -> Complex<T> {
-    let mut ret = Complex::<T>::zero();
-    let delta = T::PI() / beta;
+fn interpolate<T: MyReal>(data: &[Complex<T>], x: T, β: T, h: &impl Kernel<T>) -> Complex<T> {
+    let delta = T::PI() / β;
     let w = h.window();
     let window = w / delta.fp();
     let center = x.fp() / delta.fp();
-    let u = beta * x;
+    let u = β * x;
     let center_T = u * T::FRAC_1_PI();
 
     if (center - center.round()).abs() < 1e-11 {
         // x is very close to a grid point.
         // Assuming G is 1-Lipschitz, we can simply use the data in the grid.
-        ret = data[center as usize];
+        data[center as usize]
     } else {
+        let mut ret = Complex::<T>::zero();
         let sin_u = u.sin();
         assert!(x.fp() >= w);
         let l = (center - window).ceil() as usize;
@@ -53,31 +37,8 @@ fn interpolate<T: MyReal>(data: &[Complex<T>], x: T, beta: T, h: &impl Kernel<T>
             ret += data[n] * h.query(&cache, n)
                 / ((center_T - n as f64) * (if n % 2 == 1 { -1.0f64 } else { 1.0f64 }));
         }
-        ret = ret * sin_u * T::FRAC_1_PI();
+        ret * sin_u * T::FRAC_1_PI()
     }
-    ret
-}
-
-/// Proposition 3 in [Gourdon].
-/// We'd like to interpolate a function G(x) satisfying $|G(z) = O(e^{\tau y})|$.
-/// We have a kernel function $h(z)$ such that $h(0) = 1$ and $h(z) = o(e^{
-/// \gamma y})$. Let $\beta >= \gamma + \tau$. We have evaluations of $G(x)$ on
-/// a grid $\{x = \delta n\}_{0 \leq n < N}$ where $\delta = \pi / \beta$.
-///
-/// $w$ is the window size of $h$: $|h(w)|$ is very small so that we can
-/// safely ignore it.
-///
-/// Here we use the Sinc kernel in [Gourdon] for its efficency (Sec 2.4.3 in
-/// [Gourdon]). That is, the kernel is
-/// $h(t) = (sin(\gamma t / m) / (\gamma t/m))^m$. We choose $M$ to be a power
-/// of 2.
-pub struct BandwithInterploation<T, K: Kernel<T>> {
-    β: T,
-    δ: T,
-    γ: T,
-    m: usize,
-    data: Vec<Complex<T>>,
-    kernel: K,
 }
 
 /// `window`: Only $h(t)$ with $|t| < \text{window}$ is considered.
@@ -110,6 +71,7 @@ impl<T: MyReal> SincKernel<T> {
     }
 }
 
+/// $h(t) = (sin(t \gamma m)/(t γ \gamma m))^m$.
 impl<T: MyReal> Kernel<T> for SincKernel<T> {
     type Cache = (T, usize, (T, T));
 
@@ -119,7 +81,7 @@ impl<T: MyReal> Kernel<T> for SincKernel<T> {
         self.m = (-atol.ln() as usize).next_power_of_two();
         self.δ = δ;
         self.γ = γ;
-        let atol = atol / 10.0;  // TODO: this is an arbitrary constant...
+        let atol = atol / 10.0; // TODO: this is an arbitrary constant...
 
         let r = δ * γ / self.m as f64;
         self.log_m = self.m.trailing_zeros();
@@ -203,6 +165,41 @@ impl<T: MyReal> Kernel<T> for SinhKernel<T> {
     }
 }
 
+/// Proposition 3 in [Gourdon].
+/// We'd like to interpolate a function G(x) satisfying $|G(z) = O(e^{\tau y})|$.
+/// We have a kernel function $h(z)$ such that $h(0) = 1$ and $h(z) = o(e^{
+/// \gamma y})$. Let $\beta >= \gamma + \tau$. We have evaluations of $G(x)$ on
+/// a grid $\{x = \delta n\}_{0 \leq n < N}$ where $\delta = \pi / \beta$.
+///
+/// $w$ is the window size of $h$: $|h(w)|$ is very small so that we can
+/// safely ignore it.
+///
+/// Here we use the Sinc kernel in [Gourdon] for its efficency (Sec 2.4.3 in
+/// [Gourdon]). That is, the kernel is
+/// $h(t) = (sin(\gamma t / m) / (\gamma t/m))^m$. We choose $M$ to be a power
+/// of 2.
+pub struct BandwithInterploation<T, K: Kernel<T>> {
+    β: T,
+    δ: T,
+    γ: T,
+    m: usize,
+    data: Vec<Complex<T>>,
+    kernel: K,
+}
+
+/// a data structure for querying \sum_{t=1}^k n^{-sigma - i t}
+pub struct BandwidthInterp<T: MyReal, K: Kernel<T> = SincKernel<T>> {
+    k0: usize,
+    k1: usize,
+    σ: T,
+    t0: T,
+
+    α: T,
+    β: T,
+    data: Vec<Complex<T>>,
+    kernel: K,
+}
+
 /// TODO: refactor: this should only include bandwithinterp, no how parameters
 /// should  be selected.
 impl<T: MyReal + Sinc + ExpPolyApprox + Signed, K: Kernel<T>> BandwidthInterp<T, K> {
@@ -210,32 +207,31 @@ impl<T: MyReal + Sinc + ExpPolyApprox + Signed, K: Kernel<T>> BandwidthInterp<T,
     /// precompute = O((c + k1 eps)(tau/gap + 2))
     /// query = O(k0 + c (tau/gap + 1))
     #[inline(never)]
-    pub fn new(k: usize, min_t: T, max_t: T, sigma: T, eps: f64) -> Self {
+    pub fn new(k: usize, min_t: T, max_t: T, σ: T, eps: f64) -> Self {
         // let k0_int = std::cmp::min(4, k);
         let k0_int = 1;
         let k0 = T::mp(k0_int as f64);
         let k1 = T::mp(k as f64);
-        let tau = (k1 / k0).ln();
-        let gap = tau * 2.0;
-        let beta = tau + gap * 2.0;
-        let alpha = (k0 * k1).ln() / 2.0;
+        let τ = (k1 / k0).ln();
+        let gap = τ * 2.0;
+        let β = τ + gap * 2.0;
+        let α = (k0 * k1).ln() / 2.0;
         let max_c = T::mp(80.0);
         let t0 = min_t - max_c / gap;
         let t1 = max_t + max_c / gap;
-        let delta = T::PI() / beta;
-        let m = ((t1 - t0) / delta).to_usize().unwrap();
-        let data = sum_trunc_dirichlet(Complex::new(sigma, t0), k0_int, k, m, delta);
+        let δ = T::PI() / β;
+        let m = ((t1 - t0) / δ).fp() as usize;
+        let data = sum_trunc_dirichlet(Complex::new(σ, t0), k0_int, k, m, δ);
         let data = data
             .iter()
             .enumerate()
-            .map(|(i, &x)| Complex::from_polar(T::one(), (t0 + delta * i as f64) * alpha) * x)
+            .map(|(i, &x)| Complex::from_polar(T::one(), (t0 + δ * i as f64) * α) * x)
             .collect();
         // debug!("precompute {} terms", m);
-        // let mut h = SinhKernel::default();
-        let mut h = K::default();
-        h.init(delta, gap * 2.0, eps);
+        let mut kernel = K::default();
+        kernel.init(δ, β - τ, eps);
 
-        Self { k0: k0_int, k1: k, tau, sigma, alpha, beta, data, gap, t0, delta, h }
+        Self { k0: k0_int, k1: k, σ, α, β, data, t0, kernel }
     }
 
     /// we have all precomputed data of
@@ -244,10 +240,10 @@ impl<T: MyReal + Sinc + ExpPolyApprox + Signed, K: Kernel<T>> BandwidthInterp<T,
     pub fn query(&self, t: T) -> Complex<T> {
         let dt = t - self.t0;
 
-        let mut ret = interpolate(&self.data, dt, self.beta, &self.h);
-        ret *= Complex::from_polar(T::one(), -self.alpha * t);
+        let mut ret = interpolate(&self.data, dt, self.β, &self.kernel);
+        ret *= Complex::from_polar(T::one(), -self.α * t);
 
-        let s = Complex::new(self.sigma, t);
+        let s = Complex::new(self.σ, t);
         for n in 1..self.k0 {
             ret += (-s * T::mp(n as f64).ln()).exp();
         }
@@ -283,7 +279,7 @@ mod tests {
         for i in 1..=k {
             gt += (-s * T::mp(i as f64).ln()).exp();
         }
-        println!("ds params: alpha = {}, beta = {}, tau = {}", ds.alpha, ds.beta, ds.tau);
+        println!("ds params: alpha = {}, beta = {}", ds.α, ds.β);
 
         let output = ds.query(t);
         // let output = ds.data[0];
@@ -309,7 +305,7 @@ mod tests {
         for i in 1..=k {
             gt += (-s * T::mp(i as f64).ln()).exp();
         }
-        println!("ds params: alpha = {}, beta = {}, tau = {}", ds.alpha, ds.beta, ds.tau);
+        println!("ds params: alpha = {}, beta = {}", ds.α, ds.β);
 
         let output = ds.query(t);
         println!("gt = {}, output = {}, diff = {:.e}", gt, output, gt - output);
