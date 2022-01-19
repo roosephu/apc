3996a2222dc8c025af89d0e2b876513cc377125a
Yuping Luo <roosephu@gmail.com>
2022-01-19 16:21:41 -0500

    add APCDB Iterator

--- src/apcdb.rs
+++ src/apcdb.rs
@@ -1,9 +1,11 @@
 use crate::traits::MyReal;
 use byteorder::{LittleEndian, ReadBytesExt, WriteBytesExt};
 use log::{debug, info};
+use std::fs::File;
 use std::{
-    io::{BufReader, BufWriter},
-    path::Path,
+    io::{BufRead, BufReader, BufWriter},
+    marker::PhantomData,
+    path::{Path, PathBuf},
 };
 use F64x2::f64x2;
 
@@ -40,7 +42,7 @@ pub fn read_APCDB<T: MyReal>(
     mut f: impl FnMut(T),
 ) -> Result<(), std::io::Error> {
     let file_path = data_path.as_ref();
-    info!("Writing zeta zeros to {}", file_path.display());
+    info!("Reading zeta zeros from {}", file_path.display());
     let file = std::fs::File::open(file_path)?;
     let mut buf_reader = BufReader::new(file);
     let n0 = buf_reader.read_u64::<LittleEndian>()?;
@@ -59,9 +61,70 @@ pub fn read_APCDB<T: MyReal>(
     Ok(())
 }
 
+pub struct APCDB<T> {
+    reader: BufReader<File>,
+    n_zeros: u64,
+    atol: f64,
+    rtol: f64,
+    _marker: PhantomData<T>,
+}
+
+impl<T: MyReal> APCDB<T> {
+    pub fn new(path: impl AsRef<Path>) -> std::io::Result<Self> {
+        let path = path.as_ref();
+        let file = std::fs::File::open(&path)?;
+        let mut reader = BufReader::new(file);
+        debug!("[APCDB] loading {}", path.display());
+
+        let n0 = reader.read_u64::<LittleEndian>()?;
+        let n1 = reader.read_u64::<LittleEndian>()?;
+        let atol = reader.read_f64::<LittleEndian>()?;
+        let rtol = reader.read_f64::<LittleEndian>()?;
+
+        Ok(Self { reader, n_zeros: n1 - n0, atol, rtol, _marker: PhantomData })
+    }
+
+    pub fn directory(path: impl AsRef<Path>) -> impl Iterator<Item = (T, f64)> {
+        let path = path.as_ref().to_path_buf();
+        let file = std::fs::File::open(path.join("md5.txt")).unwrap();
+        let reader = std::io::BufReader::new(file);
+        let mut indices = vec![];
+        for line in reader.lines() {
+            let line = line.unwrap();
+            let index = &line[41..line.len() - 4];
+            let index = index.parse::<i64>().unwrap();
+            indices.push(index);
+        }
+
+        indices
+            .into_iter()
+            .flat_map(move |index| Self::new(path.join(format!("height_{index}.dat"))).unwrap())
+    }
+}
+
+impl<T: MyReal> Iterator for APCDB<T> {
+    type Item = (T, f64);
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let reader = &mut self.reader;
+        let mut inner = || {
+            if self.n_zeros == 0 {
+                return None;
+            }
+            let hi = reader.read_f64::<LittleEndian>().ok()?;
+            let lo = reader.read_f64::<LittleEndian>().ok()?;
+            let z = T::zero() + hi + lo;
+
+            self.n_zeros -= 1;
+            Some((z, z.fp() * self.rtol + self.atol))
+        };
+        inner()
+    }
+}
+
 #[cfg(test)]
 mod tests {
-    use super::{read_APCDB, write_APCDB};
+    use super::{read_APCDB, write_APCDB, APCDB};
     use crate::lmfdb::read_LMFDB;
     use crate::zeta_zeros::{try_isolate, HybridPrecHardyZ};
 
@@ -98,11 +161,13 @@ mod tests {
     #[test]
     fn check_read_apcdb() {
         crate::init();
+        let limit = 1e4;
+
         let mut apcdb_zeros = vec![];
-        let _ = read_APCDB("data/apcdb/height_100000.dat", 1e4, |x: f64x2| apcdb_zeros.push(x));
+        let _ = read_APCDB("data/apcdb/height_100000.dat", limit, |x: f64x2| apcdb_zeros.push(x));
 
         let mut lmfdb_zeros = vec![];
-        let _ = read_LMFDB("data/lmfdb", 1e4, |x: f64x2| lmfdb_zeros.push(x));
+        let _ = read_LMFDB("data/lmfdb", limit, |x: f64x2| lmfdb_zeros.push(x));
 
         let n1 = apcdb_zeros.len();
         let n2 = lmfdb_zeros.len();
@@ -111,5 +176,10 @@ mod tests {
             let (r1, r2) = (apcdb_zeros[i], lmfdb_zeros[i]);
             assert_close(r1, r2, 1e-18, 1e-18);
         }
+
+        let apcdb2 = APCDB::<f64x2>::directory("data/apcdb")
+            .map_while(|(x, _)| if x.fp() <= limit { Some(x) } else { None })
+            .collect::<Vec<_>>();
+        assert!(apcdb_zeros == apcdb2);
     }
 }
